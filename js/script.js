804174dec48fe26fb4bb88c15b8a5e3a5548bc12
Henrique Teixeira <henriqueteixeira.wd@gmail.com>
2023-01-17 04:21:18 -0300

    Update202301170421

--- js/script.js
+++ js/script.js
@@ -1,18 +1,5 @@
-<script>
-
-function mudouTamanho() {
-    if (window.innerWidth >= 768) {
-        itens.style.display = 'block'
-    } else {
-        itens.style.display = 'none'
-    }
-}
-
-function clickMenu() {
-    if (itens.style.display == 'block') {
-        itens.style.display = 'none'
-    } else {
-        itens.style.display = 'block'
-    }
-}
-</script>
+/*  Menu Scroll  */
+const header = document.querySelector("header");
+window.addEventListener ("scroll", function() {
+    header.classList.toggle ("sticky", window.scrollY > 100);
+});
